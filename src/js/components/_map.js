26f607e4033b5a729118306093325e5cebd4cf1e
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-09-26 13:05:53 +0300

    map

--- src/js/components/_map.js
+++ src/js/components/_map.js
@@ -1,15 +1,197 @@
+export default class Map {
 
-// function myMap() {
-//   var mapOptions = {
-//     center: new google.maps.LatLng(51.5, -0.12),
-//     zoom: 10,
-//     mapTypeId: google.maps.MapTypeId.HYBRID, 
-//     APIKEY: 'AIzaSyDMWIxCN9ijYRfiH7bmQN-LNRDtoboLZqY'
-//   };
-//   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-// }
+  constructor(config) {
+    this._mapHTML = config.map.get(0);
+    this._center = {
+      lat: +config.map.data('center-lat'),
+      lng: +config.map.data('center-lng')
+    };
+    this._marker = {
+      lat: +config.map.data('marker-lat'),
+      lng: +config.map.data('marker-lng')
+    };
 
-// window.myMap = myMap;
-// myMap();
+   
+    this._init();
+  }
+   
+  _init() {
+    this._createMap();
+  }
+   
+  _createMap() {
+    this._map = new google.maps.Map(this._mapHTML, {
+      center: this._center,
+      zoom: 15,
+      // disableDefaultUI: true,
+      // scrollwheel: false,
+      // zoomControl: true,
+      // draggable: false,
+    //   styles: [
+    //     {
+    //       'featureType': 'administrative.province',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'off'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'administrative.locality',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'off'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'administrative.neighborhood',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'off'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'administrative.land_parcel',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'off'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'administrative.land_parcel',
+    //       'elementType': 'geometry',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'off'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'poi',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'off'
+    //         }
+    //       ]
+    //     },
+    //     // {
+    //     //   'featureType': 'transit',
+    //     //   'elementType': 'all',
+    //     //   'stylers': [
+    //     //     {
+    //     //       'visibility': 'on'
+    //     //     },
+    //     //     {
+    //     //       'weight': '1.68'
+    //     //     },
+    //     //     {
+    //     //       'hue': '#006bff'
+    //     //     }
+    //     //   ]
+    //     // },
+    //     {
+    //       'featureType': 'transit',
+    //       'elementType': 'geometry.fill',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'on'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'transit',
+    //       'elementType': 'geometry.stroke',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'on'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'transit',
+    //       'elementType': 'labels',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'on'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'transit',
+    //       'elementType': 'labels.icon',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'on'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'transit.station',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'off'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'transit.station.airport',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'on'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'transit.station.bus',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'off'
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       'featureType': 'transit.station.rail',
+    //       'elementType': 'all',
+    //       'stylers': [
+    //         {
+    //           'visibility': 'on'
+    //         }
+    //       ]
+    //     }
+    //   ]
+    });
+    new google.maps.Marker({
+      position: this._marker,
+      map: this._map,
+      title: 'Пресненская набережная, дом 8, стр. 1'
+    //   label: 'Bamboo Bar'
+    //   icon: {
+    //     // url: 'img/marker.png',
+    //     scaledSize: new google.maps.Size(50, 57)
+    //   }
+    });
+  }
+   
+};
 
-var GoogleMapsLoader = require('google-maps');
+let map = $('.js-map');
+
+if (map.length) {
+  map.each(function() {
+    new Map({
+      map: $(this)
+    });
+  });
+};
+
